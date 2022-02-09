1d88a246987f818923d93be7656cfd530d065453
Inigo Alvarez echarri <ialvarez@c1r8s5.42madrid.com>
2022-02-09 19:58:03 +0100

    su

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ialvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 19:47:20 by ialvarez          #+#    #+#             */
-/*   Updated: 2022/02/08 22:05:22 by ialvarez         ###   ########.fr       */
+/*   Updated: 2022/02/09 19:57:39 by ialvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,66 +73,92 @@ void	error(t_stack **a)
 
 }
 */
+static char **keep(char **argv, int argc)
+{
+	char **aux;
+	int i;
+	int j;
+
+	j = 1;
+	i = 2;
+	aux = NULL;
+	while (i++ <= argc)
+	{
+		aux = ft_split(argv[j], ' ');
+		j++;
+	}
+	return (aux);
+}
 
 static void parseo(char **argv)
 {
 	int i;
 	int j;
+	long n;
 
 	i = 0;
+	n = 0;
 	while (argv[i])
 	{
 		j = 0;
+		printf("parse%s\n\n", argv[i]);
 		while(argv[i][j])
 		{
 			if ((argv[i][j] == '-' && ft_isdigit(argv[i][j + 1])) || 
-					(argv[i][j] == '+' && ft_isdigit(argv[i][j])))
+					(argv[i][j] == '+' && ft_isdigit(argv[i][j + 1])))
 				j++;
 			else if (!ft_isdigit(argv[i][j]))
 			{
-
-			//ft_error(tak);
 				write(1, "Error\n", 6);
-				//printf("asqwwqqw%s\n", argv[i]);
 				exit (0);
 			}
 			j++;
 		}
+		n = ft_atoi(argv[i]);
+		if(n < -2147483648 || n > 2147483647)
+		{
+			write(1, "Error\n", 6);
+			exit (0);
+		}
 		i++;
 	}
-		/*	
-	if (num > 2147483647 || num < -2147483648)
-	{
-		write (1, "Error", 6);
-		free(argv);
-		exit (0);
-		*/
 }
-static void		keep_data(char *argv)
+static void		look_data(char *argv)
 {
 	char	**tak;
 
 	tak = ft_split(argv, ' ');
 		printf("hello%s\n", tak[0]);
 	parseo(tak);
+	printf("lola%s\n", tak[0]);
 }
 
 int		main(int argc, char **argv)
 {
 	t_stack	*a;
 	t_stack	*b;
+	char **aux;
 	//int	p;
+//	int		o = 0;
 	int		i = 2;
 	int		j = 1;
 
 	a = NULL;
 	b = NULL;
+	aux = NULL;
 	if (argc <= 1)
 		return (0);
 	else
 	{
-		while(i++ <= argc) //tengo que mirar el numero de numeros que hay en la lista, y guardarlo
-			keep_data(argv[j++]);
+		while(i++ <= argc)
+			look_data(argv[j++]);
+		aux = keep(argv, argc);
+		printf("keepp---> %s\n", aux[0]);
+		printf("keepp---> %s\n", aux[1]);
+		//printf("keepp---> %s\n", aux[2]);
+	//	printf("keepp---> %s\n", aux[3]);
+	//	printf("keepp---> %s\n", aux[4]);
+		
 		//a = init(&aux);
 		//printf("%d\n", argc);
 		//si = ft_strlen(tak);
